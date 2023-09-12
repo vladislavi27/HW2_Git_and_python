deb809ba08351e2e736ae27978a4643f418db2f5
estakathersun <ksum.miha@gmail.com>
2023-09-12 19:22:41 +0300

    Add complete main function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,20 @@
+# input for calculation
 task = input()
 task = task.split(" ")
+
+
+# definition of functions
 def main(task):
+    num1, num2 = float(task[0]), float(task[2])
+    mode = task[1]
+    if mode == '+':
+        return addition(num1, num2)
+    elif mode == '-':
+        return substraction(num1, num2)
+    elif mode == '*':
+        return multiplication(num1, num2)
+    elif mode == '/':
+        return division(num1, num2)
+
+main (task)
+
